7fe450f572390a3e5d27f1bb898c6a76e1f5329d
Etienne Callies <etienne.callies@ouihelp.fr>
2023-03-29 12:07:20 +0200

    stream calls with transcript ok

--- airbyte-integrations/connectors/source-modjo/source_modjo/source.py
+++ airbyte-integrations/connectors/source-modjo/source_modjo/source.py
@@ -3,84 +3,12 @@
 #
 from typing import Any, List, Mapping, Tuple
 
-import pendulum
 import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from requests.auth import AuthBase
 
+from source_modjo.authenticator import ModjoAuthenticator
 from source_modjo.streams import Calls
-
-
-# Authentication
-class ModjoOauth2(AuthBase):
-    """
-    Generates OAuth2.0 access tokens from an OAuth2.0 refresh token and client credentials.
-    The generated access token is attached to each request via the Authorization header.
-    """
-    MODJO_TOKEN_DURATION = 15 * 60
-
-    def __init__(
-        self,
-        refresh_token: str
-    ):
-        self.refresh_token = refresh_token
-        self._token_expiry_date = None
-        self._access_token = None
-
-    def __call__(self, request):
-        request.headers.update(self.get_auth_header())
-        return request
-
-    def get_auth_header(self) -> Mapping[str, Any]:
-        return {"Authorization": f"Bearer {self.get_access_token()}"}
-
-    def get_access_token(self):
-        if self.token_has_expired():
-            t0 = pendulum.now()
-            access_token, refresh_token = self.refresh_access_token()
-            self.refresh_token = refresh_token
-            self._access_token = access_token
-            self._token_expiry_date = t0.add(seconds=self.MODJO_TOKEN_DURATION)
-
-        return self._access_token
-
-    def token_has_expired(self) -> bool:
-        return self._token_expiry_date is None or pendulum.now() > self._token_expiry_date
-
-    def refresh_access_token(self) -> Tuple[str, str]:
-        """
-        returns a tuple of (access_token, token_lifespan_in_seconds)
-        """
-        token_refresh_endpoint = 'https://api.modjo.ai/auth/refresh'
-        try:
-            response = requests.request(method="POST", url=token_refresh_endpoint, data={"token": self.refresh_token})
-            response.raise_for_status()
-            response_json = response.json()
-            return response_json['accessToken'], response_json['refreshToken']
-        except Exception as e:
-            raise Exception(f"Error while refreshing access token: {e}") from e
-
-
-class ModjoAuthenticator:
-    def get_auth(self, config: Mapping[str, Any]) -> ModjoOauth2:
-        email = config.get("email")
-        if not email:
-            raise Exception("No email")
-
-        password = config.get("password")
-        if not password:
-            raise Exception("No password")
-
-        response = requests.post(f"https://api.modjo.ai/auth/signin", json={
-            'email': email,
-            'password': password,
-        })
-        response_data = response.json()
-
-        auth = ModjoOauth2(response_data['refreshToken'])
-
-        return auth
 
 
 # Source
--- airbyte-integrations/connectors/source-modjo/source_modjo/streams.py
+++ airbyte-integrations/connectors/source-modjo/source_modjo/streams.py
@@ -1,31 +1,38 @@
 #
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
-from abc import abstractmethod
 from typing import Any, Iterable, Mapping, MutableMapping, Optional
 
 import requests
 from airbyte_cdk.sources.streams.http import HttpStream
+from source_modjo.authenticator import ModjoOauth2
 
 
 # Basic full refresh stream
-class ModjoStream(HttpStream):
+class Calls(HttpStream):
     url_base = "https://api.modjo.ai/"
     per_page = 20
 
     primary_key = 'id'
+    data_field = 'values'
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.current_page = 0
+    def path(self, **kwargs):
+        return 'calls/list'
+
+    def __init__(self, authenticator: ModjoOauth2):
+        super().__init__(authenticator)
+        self.current_page = 1
+        self._authenticator = authenticator
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         decoded_response = response.json()
         api_data = decoded_response[self.data_field]
 
         if len(api_data) < self.per_page:
-            self.current_page = 0
+            self.current_page = 1
             return None
+
+        self.current_page += 1
 
         return {"page": self.current_page}
 
@@ -36,7 +43,7 @@ class ModjoStream(HttpStream):
         next_page_token: Mapping[str, Any] = None,
     ) -> MutableMapping[str, Any]:
 
-        params = {"per_page": self.per_page}
+        params = {"perPage": self.per_page}
 
         # Handle pagination by inserting the next page's token in the request parameters
         if next_page_token:
@@ -45,18 +52,14 @@ class ModjoStream(HttpStream):
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         response_json = response.json()
-        yield from response_json[self.data_field]
+        records = response_json[self.data_field]
 
-    @property
-    @abstractmethod
-    def data_field(self) -> str:
-        """The response entry that contains useful data"""
-        pass
+        for record in records:
+            # Download transcript for each call
+            record_id = record['id']
+            transcript_url = f'https://api.modjo.ai/transcription-segments/{record_id}?format=text%2Fsrt'
+            sub_response = requests.get(transcript_url, headers=self._authenticator.get_auth_header())
+            record['transcript'] = str(sub_response.text)
 
-
-class Calls(ModjoStream):
-    data_field = 'values'
-
-    def path(self, **kwargs) -> str:
-        return 'calls/list'
+            yield record
 
